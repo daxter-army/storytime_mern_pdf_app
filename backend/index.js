ef0ab6ad05046a6196673ca8c4d19c7956aff6be
daxter-army <mehulsaini456@gmail.com>
2021-05-09 14:41:35 +0530

    updated gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log('hello')
+import express from 'express'
+
+const app = express()
+
+app.get('/serve', (req, res) => {
+    res.status(200).send('hello')
+})
+
+app.listen(5000, () => {
+    console.log('listening')
+})
